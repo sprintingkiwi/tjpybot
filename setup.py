711d3f56a96ed89862d4bee589e5615275c1dbbd
Alessandro Norfo <sprintingkiwi@gmail.com>
2018-02-28 21:14:42 +0100

    added setup file for pip package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='tjpybot',
+    packages=['tjpybot'],
+    version='0.1',
+    description='A playful way to learn coding with Python and TJBot',
+    author='Alessandro Norfo',
+    author_email='sprintingkiwi@gmail.com',
+    url='https://github.com/sprintingkiwi/tjpybot',
+    download_url='https://github.com/sprintingkiwi/tjpybot/tarball/0.1',
+    keywords=['learning', 'education', 'TJBot', 'robotics'],
+    classifiers=[]
+)
