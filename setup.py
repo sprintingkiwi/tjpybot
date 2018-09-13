59a86560576e3acd7cb2ad1e9fb4aa1a7f5d80aa
Alessandro Norfo <ale.norfo@gmail.com>
2018-09-13 16:03:13 +0200

    v0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='tjpybot',
     packages=['tjpybot'],
-    version='0.1',
+    version='0.2',
     description='A playful way to learn coding with Python and TJBot',
     author='ASPHI Onlus',
     author_email='sprintingkiwi@gmail.com',
